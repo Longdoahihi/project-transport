8c8f0b586138c624e544b0ee50c5526c7ba6d192
Quách Văn Long <75673478+Longdoahihi@users.noreply.github.com>
2023-04-06 01:35:58 +0700

    project constructor

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -3,22 +3,11 @@ const express = require('express');
 const { engine } = require('express-handlebars');
 const morgan = require('morgan');
 const app = express();
-//aws
-const AWS = require('aws-sdk');
-let S3 = new AWS.S3({
-    region: 'us-east-1',
-    accessKeyId: 'AKIASHZWWJEY4RKHBKVM',
-    secretAccessKey: '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'
-})
-S3.createBucket({
-    Bucket: 'demo-bucket-created'
-},(err)=>{
-    console.log("err: ",err)
-})
-//config routes
+
+//config routesx
 const route = require('./resources/routes')
 // static store img
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 //HTTP logger
 app.use(morgan('combined'))
 //template engine
@@ -26,7 +15,7 @@ app.engine('hbs', engine({
     extname: '.hbs'
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'resources/views'));
+app.set('views', path.join(__dirname, 'resources/views'));
 
 route(app);
 
